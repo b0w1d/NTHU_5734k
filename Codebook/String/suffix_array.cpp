d0e4861e3bdf340fc8c7728b00b9d92796c94a02
b0w1d <61a5t0@gmail.com>
2018-02-11 01:12:49 +0800

    Added SA-IS

---
+++ Codebook/String/suffix_array.cpp
@@ -0,0 +1,132 @@
+// -----------O(NlgNlgN)----------
+struct suffix_array {
+  string s;
+  int n;
+  vector<int> sa;
+  suffix_array(const string &_s) : s(_s), n(s.size()), sa(n) {
+    vector<int> r(n), t(n);
+    for (int i = 0; i < n; ++i) r[sa[i] = i] = s[i];
+    for (int h = 1; t[n - 1] != n - 1; h *= 2) {
+      auto cmp = [&](int i, int j) {
+        if (r[i] != r[j]) return r[i] < r[j];
+        return i + h < n && j + h < n ? r[i + h] < r[j + h] : i > j;
+      };
+      sort(sa.begin(), sa.end(), cmp);
+      for (int i = 0; i + 1 < n; ++i) t[i + 1] = t[i] + cmp(sa[i], sa[i + 1]);
+      for (int i = 0; i < n; ++i) r[sa[i]] = t[i];
+    }
+  }
+  int operator[](int i) const { return sa[i]; }
+};
+
+// ------------O(N)-------------
+bool is_lms(const string &t, int i) {
+  return i > 0 && t[i - 1] == 'L' && t[i] == 'S';
+}
+
+template<typename T>
+vector<int> induced_sort(const T &s, const string &t, const vector<int> &lmss, int sigma = 256) {
+  vector<int> sa(s.size(), -1);
+
+  vector<int> bin(sigma + 1);
+  for (auto it = s.begin(); it != s.end(); ++it) {
+    ++bin[*it + 1];
+  }
+
+  int sum = 0;
+  for (int i = 0; i < bin.size(); ++i) {
+    sum += bin[i];
+    bin[i] = sum;
+  }
+
+  vector<int> cnt(sigma);
+  for (auto it = lmss.rbegin(); it != lmss.rend(); ++it) {
+    int ch = s[*it];
+    sa[bin[ch + 1] - 1 - cnt[ch]] = *it;
+    ++cnt[ch];
+  }
+
+  cnt = vector<int>(sigma);
+  for (auto it = sa.begin(); it != sa.end(); ++it) {
+    if (*it <= 0 || t[*it - 1] == 'S') continue;
+    int ch = s[*it - 1];
+    sa[bin[ch] + cnt[ch]] = *it - 1;
+    ++cnt[ch];
+  }
+
+  cnt = vector<int>(sigma);
+  for (auto it = sa.rbegin(); it != sa.rend(); ++it) {
+    if (*it <= 0 || t[*it - 1] == 'L') continue;
+    int ch = s[*it - 1];
+    sa[bin[ch + 1] - 1 - cnt[ch]] = *it - 1;
+    ++cnt[ch];
+  }
+
+  return sa;
+}
+
+template<typename T>
+vector<int> sa_is(const T &s, int sigma = 256) {
+  string t(s.size(), 0);
+  t[s.size() - 1] = 'S';
+  for (int i = int(s.size()) - 2; i >= 0; --i) {
+    if (s[i] < s[i + 1]) t[i] = 'S';
+    else if (s[i] > s[i + 1]) t[i] = 'L';
+    else t[i] = t[i + 1];
+  }
+
+  vector<int> lmss;
+  for (int i = 0; i < s.size(); ++i) {
+    if (is_lms(t, i)) {
+      lmss.emplace_back(i);
+    }
+  }
+
+  vector<int> sa = induced_sort(s, t, lmss, sigma);
+  vector<int> sa_lms;
+  for (int i = 0; i < sa.size(); ++i) {
+    if (is_lms(t, sa[i])) {
+      sa_lms.emplace_back(sa[i]);
+    }
+  }
+
+  int lmp_ctr = 0;
+  vector<int> lmp(s.size(), -1);
+  lmp[sa_lms[0]] = lmp_ctr;
+  for (int i = 0; i + 1 < sa_lms.size(); ++i) {
+    int diff = 0;
+    for (int d = 0; d < sa.size(); ++d) {
+      if (s[sa_lms[i] + d] != s[sa_lms[i + 1] + d] ||
+          is_lms(t, sa_lms[i] + d) != is_lms(t, sa_lms[i + 1] + d)) {
+        diff = 1; // something different in range of lms
+        break;
+      } else if (d > 0 && is_lms(t, sa_lms[i] + d) && is_lms(t, sa_lms[i + 1] + d)) {
+        break; // exactly the same
+      }
+    }
+    if (diff) ++lmp_ctr;
+    lmp[sa_lms[i + 1]] = lmp_ctr;
+  }
+
+  vector<int> lmp_compact;
+  for (int i = 0; i < lmp.size(); ++i) {
+    if (~lmp[i]) {
+      lmp_compact.emplace_back(lmp[i]);
+    }
+  }
+
+  if (lmp_ctr + 1 < lmp_compact.size()) {
+    sa_lms = sa_is(lmp_compact, lmp_ctr + 1);
+  } else {
+    for (int i = 0; i < lmp_compact.size(); ++i) {
+      sa_lms[lmp_compact[i]] = i;
+    }
+  }
+
+  vector<int> seed;
+  for (int i = 0; i < sa_lms.size(); ++i) {
+    seed.emplace_back(lmss[sa_lms[i]]);
+  }
+
+  return induced_sort(s, t, seed, sigma);
+} // Usage: you have s, then sa = sa_is(s + char(0)), c > 0 for all c in s
