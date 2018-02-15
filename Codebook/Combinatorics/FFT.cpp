508cc529e1e64226950277d072c585ff6faa0a3c
rareone <rareone0602@me.com>
2018-02-15 21:20:29 +0800

    make

--- Codebook/Combinatorics/FFT.cpp
+++ Codebook/Combinatorics/FFT.cpp
@@ -1,8 +1,7 @@
 typedef complex<double> cpx;
-int lg = 18;
 const double PI = acos(-1);
 vector<cpx> FFT(vector<cpx> &P, bool inv = 0) {
-  int n = 1 << lg; // == P.size();
+  int lg = 31 - __builtin_clz(P.size()), n = 1 << lg; // == P.size();
   for (int j = 1, i = 0; j < n - 1; ++j) {
     for (int k = n >> 1; k > (i ^= k); k >>= 1);
     if (j < i) swap(P[i], P[j]);
