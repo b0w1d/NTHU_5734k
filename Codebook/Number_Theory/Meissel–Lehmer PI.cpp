9293982f378e76ab27d8c6a8578ce2a7356de2f5
rareone <rareone0602@me.com>
2018-02-16 13:31:38 +0800

    Update Meissel–Lehmer PI.cpp
    bug fixed and correctness

--- Codebook/Number_Theory/Meissel–Lehmer PI.cpp
+++ Codebook/Number_Theory/Meissel–Lehmer PI.cpp
@@ -1,9 +1,8 @@
 LL PI(LL m);
-const int MAXM = 10000, MAXN = 650, UPBD = 5000000;
+const int MAXM = 1000, MAXN = 650, UPBD = 1000000;
 // 650 ~ PI(cbrt(1e11))
 LL pi[UPBD] = {0}, phi[MAXM][MAXN];
 vector<LL> primes;
-
 void init() {
   fill(pi + 2, pi + UPBD, 1);
   for (LL p = 2; p < UPBD; ++p) 
@@ -19,30 +18,23 @@ void init() {
     for (int j = 1; j < MAXN; ++j)
       phi[i][j] = phi[i][j - 1] - phi[i / primes[j - 1]][j - 1];
 }
-
 LL P_2(LL m, LL n) {
   LL ans = 0;
   for (LL i = n; primes[i] * primes[i] <= m and i < primes.size(); ++i) 
     ans += PI(m / primes[i]) - i;
   return ans;
 }
-
 LL PHI(LL m, LL n) {
   if (m < MAXM and n < MAXN) return phi[m][n];
   if (n == 0) return m;
-  auto p = primes[n - 1];
+  LL p = primes[n - 1];
   if (m < UPBD) {
+    if (m <= p) return 1;
     if (m <= p * p) return pi[m] - n + 1;
-    if (m <= p * p * p) {
-      int s2n = pi[(int)sqrt(0.5 + m)];
-      LL ans = pi[m] - (s2n + n - 2) * (s2n - n + 1) / 2;
-      for(int i = n; i < s2n; i++) ans += pi[m / primes[i]];
-      return ans;
-    }
+    if (m <= p * p * p) return pi[m] - n + 1 + P_2(m, n);
   }
   return PHI(m, n - 1) - PHI(m / p, n - 1);
 }
-
 LL PI(LL m) {
   if (m < UPBD) return pi[m];
   LL y = cbrt(m) + 10, n = pi[y];
