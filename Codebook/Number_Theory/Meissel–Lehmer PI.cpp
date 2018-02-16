744c5c24588e47ae063805996df475db422e0054
rareone <rareone0602@me.com>
2018-02-16 12:48:58 +0800

    Update Meissel–Lehmer PI.cpp
    test by hdu 5091
    up to 1e12

--- Codebook/Number_Theory/Meissel–Lehmer PI.cpp
+++ Codebook/Number_Theory/Meissel–Lehmer PI.cpp
@@ -1,6 +1,7 @@
 LL PI(LL m);
-const int MAXN = 5000, UPBD = MAXN * MAXN;
-LL pi[UPBD] = {0}, phi[MAXN][MAXN];
+const int MAXM = 10000, MAXN = 650, UPBD = 5000000;
+// 650 ~ PI(cbrt(1e11))
+LL pi[UPBD] = {0}, phi[MAXM][MAXN];
 vector<LL> primes;
 void init() {
   fill(pi + 2, pi + UPBD, 1);
@@ -11,9 +12,9 @@ void init() {
       primes.push_back(p);
     }
   for (int i = 1; i < UPBD; ++i) pi[i] += pi[i - 1];
-  for (int i = 0; i < MAXN; ++i) 
-    phi[i][0] = i, phi[1][i] = 1;
-  for (int i = 2; i < MAXN; ++i) 
+  for (int i = 0; i < MAXM; ++i) 
+    phi[i][0] = i;
+  for (int i = 1; i < MAXM; ++i) 
     for (int j = 1; j < MAXN; ++j)
       phi[i][j] = phi[i][j - 1] - phi[i / primes[j - 1]][j - 1];
 }
@@ -23,12 +24,23 @@ LL P_2(LL m, LL n) {
     ans += PI(m / primes[i]) - i;
   return ans;
 }
+
 LL PHI(LL m, LL n) {
-  if (m < MAXN and n < MAXN) return phi[m][n];
+  if (m < MAXM and n < MAXN) return phi[m][n];
   if (n == 0) return m;
-  return PHI(m, n - 1) - PHI(m / primes[n - 1], n - 1);
+  auto p = primes[n - 1];
+  if (m < UPBD) {
+    if (m <= p * p) return pi[m] - n + 1;
+    if (m <= p * p * p) {
+      int s2n = pi[(int)sqrt(0.5 + m)];
+      LL ans = pi[m] - (s2n + n - 2) * (s2n - n + 1) / 2;
+      for(int i = n; i < s2n; i++) ans += pi[m / primes[i]];
+      return ans;
+    }
+  }
+  return PHI(m, n - 1) - PHI(m / p, n - 1);
 }
-LL PI(LL m) { // m * m > UPBD
+LL PI(LL m) {
   if (m < UPBD) return pi[m];
   LL y = cbrt(m) + 10, n = pi[y];
   return PHI(m, n) + n - 1 - P_2(m, n);
