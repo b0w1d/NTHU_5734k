6f2dc0b4befddd57a25d78b0548b9a02c066f1dd
rareone <rareone0602@me.com>
2018-02-18 00:35:13 +0800

    Create basic.cpp

---
+++ Codebook/Geometry/basic.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+typedef complex<double> pt;
+double cross(pt A, pt B) {
+	return A.real() * B.imag() - A.imag() * B.real();
+}
+double dot(pt A, pt B) {
+	return A.real() * B.real() + B.imag() * A.imag();
+}
