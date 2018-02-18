282495a99c5a69a8be8d9279b964990301bba7c2
b0w1d <61a5t0@gmail.com>
2018-02-18 23:00:13 +0800

    Fixed typo

--- Codebook/String/gusfield.cpp
+++ Codebook/String/gusfield.cpp
@@ -1,7 +1,7 @@
 template<typename T>
 vector<int> gusfield(const T &s) {
-  vector<int> z(s.size()); // z[i] := max k for z[0, k) = z[i, i + k), i > 0
-  for (int i = 1, L = 0, R = 0, i < s.size(); ++i) {
+  vector<int> z(s.size(), s.size()); // z[i] := max k for z[0, k) = z[i, i + k)
+  for (int i = 1, L = 0, R = 0; i < s.size(); ++i) {
     if (R < i) {
       L = R = i;
       while (R < s.size() && s[R] == s[R - L]) ++R;
