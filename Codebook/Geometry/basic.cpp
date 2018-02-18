1094980b46465f21ed01e331a343d49ea0a083cc
rareone <rareone0602@me.com>
2018-02-18 11:33:43 +0800

    reindent

--- Codebook/Geometry/basic.cpp
+++ Codebook/Geometry/basic.cpp
@@ -1,7 +1,6 @@
-using namespace std;
 typedef complex<double> pt;
 double cross(pt A, pt B) {
-	return A.real() * B.imag() - A.imag() * B.real();
+  return A.real() * B.imag() - A.imag() * B.real();
 }
 double dot(pt A, pt B) {
 	return A.real() * B.real() + B.imag() * A.imag();
