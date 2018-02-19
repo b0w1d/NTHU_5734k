f4cb7137c622ec6f1d13ae3e77b978849004e9d4
rareone <rareone0602@me.com>
2018-02-19 08:59:08 +0800

    Update basic.cpp

--- Codebook/Geometry/basic.cpp
+++ Codebook/Geometry/basic.cpp
@@ -3,5 +3,5 @@ double cross(pt A, pt B) {
   return A.real() * B.imag() - A.imag() * B.real();
 }
 double dot(pt A, pt B) {
-	return A.real() * B.real() + B.imag() * A.imag();
+  return A.real() * B.real() + B.imag() * A.imag();
 }
