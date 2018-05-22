1bb7f8addec43a08fe06eb11ae6608efdc51b505
rareone <rareone0602@me.com>
2018-05-23 03:25:40 +0800

    bugs in Gauss_Lagrange_Eisenstein_reduced_form

--- Codebook/Math/Gauss_Lagrange_Eisenstein_reduced_form.cpp
+++ Codebook/Math/Gauss_Lagrange_Eisenstein_reduced_form.cpp
@@ -7,8 +7,9 @@
   if (a > c) return tie(x, y) = form(c, b, a), {y, x};
   if (a == c and b < 0) return tie(x, y) = form(a, -b, c), {-x, y};
   if (b > a or b <= -a) {
-    LL n = (a - b) / (2 * a), x, y;
-    if (2 * a * n <= a - b) --n;
+    LL n = (a - b) / (2 * a);
+    // -a < 2 * a * n + b <= a
+    if (2 * a * n > a - b) --n;
     tie(x, y) = form(a, 2 * a * n + b, a * n * n + b * n + c);
     return {x - n * y, y};
   }
