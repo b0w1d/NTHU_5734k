43c8db3dda9683f677e277bc6f4412678b59578f
rareone <rareone0602@me.com>
2018-07-02 23:44:03 +0800

    update BCC and add BCC picture illustration

--- Codebook/Graph/BCC.cpp
+++ Codebook/Graph/BCC.cpp
@@ -1,45 +1,34 @@
-
-struct BccVertex {
-  int n,nScc,step,dfn[MXN],low[MXN];
-  vector<int> E[MXN],sccv[MXN];
-  int top,stk[MXN];
-  void init(int _n) {
-    n = _n; nScc = step = 0;
-    for (int i=0; i<n; i++) E[i].clear();
+struct BCC_solver{
+  vector<vector<int>> g, BCC; // BCC are the components
+  vector<int> dfn, low, stk;
+  int step = 0;
+  void init(int n) {
+    g.assign(n, vector<int>(0)), BCC.clear();
+    stk.clear(), step = 0;
+    dfn.assign(n, -1), low.assign(n, -1);
   }
-  void addEdge(int u, int v)
-  { E[u].PB(v); E[v].PB(u); }
-  void DFS(int u, int f) {
+  void add_edge(int a, int b) { g[a].push_back(b), g[b].push_back(a); }
+  void dfs(int u, int p = -1) {
     dfn[u] = low[u] = step++;
-    stk[top++] = u;
-    for (auto v:E[u]) {
-      if (v == f) continue;
+    stk.push_back(u);
+    for (int v: g[u]) {
+      if (v == p) continue;
       if (dfn[v] == -1) {
-        DFS(v,u);
+        dfs(v, u);
         low[u] = min(low[u], low[v]);
-        if (low[v] >= dfn[u]) {
-          int z;
-          sccv[nScc].clear();
-          do {
-            z = stk[--top];
-            sccv[nScc].PB(z);
-          } while (z != v);
-          sccv[nScc++].PB(u);
-        }
-      }else
-        low[u] = min(low[u],dfn[v]);
+      } else low[u] = min(low[u], dfn[v]);
+    }
+    if (low[u] == dfn[u]) {
+      BCC.push_back(vector<int>(0));
+      while (stk.back() != u)
+        BCC.back().push_back(stk.back()),
+        stk.pop_back();
+      BCC.back().push_back(u);
+      stk.pop_back();
     }
   }
-  vector<vector<int>> solve() {
-    vector<vector<int>> res;
-    for (int i=0; i<n; i++)
-      dfn[i] = low[i] = -1;
-    for (int i=0; i<n; i++)
-      if (dfn[i] == -1) {
-        top = 0;
-        DFS(i,i);
-      }
-    REP(i,nScc) res.PB(sccv[i]);
-    return res;
+  vector<vector<int>> find_BCC() {
+    dfs(0);
+    return BCC;
   }
-}graph;
+}bcc_solver;
