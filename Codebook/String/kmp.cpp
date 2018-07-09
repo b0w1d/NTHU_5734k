7eb4d5552eec86a480198c3a32175db292148694
rareone <rareone0602@me.com>
2018-07-10 02:58:27 +0800

    add KMP application

--- Codebook/String/kmp.cpp
+++ Codebook/String/kmp.cpp
@@ -9,3 +9,14 @@ vector<int> build_kmp(const T &s) {
   }
   return f;
 }
+template<typename S>
+vector<int> kmp_match(vector<int> fail, const S &P, const S &T) {
+  vector<int> res; // start from these points
+  const int n = P.size();
+  for (int j = 0, i = -1; j < T.size(); ++j) {
+    while (~i and T[j] != P[i + 1]) i = fail[i];
+    if (P[i + 1] == T[j]) ++i;
+    if (i == n - 1) res.push_back(j - n + 1), i = fail[i];
+  }
+  return res;
+}
