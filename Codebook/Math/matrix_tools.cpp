4d5f711a0d82537bba652acc347964e3db3dfa98
rareone <rareone0602@me.com>
2018-07-21 14:31:04 +0800

    寫題目更新日常zzz

--- Codebook/Math/matrix_tools.cpp
+++ Codebook/Math/matrix_tools.cpp
@@ -1,22 +1,26 @@
 typedef vector<vector<LL>> mat;
-const int dim = 2; // dimension;
-const mat Id = {{1, 0}, {0, 1}}; // modify it if diemnsion != 2
-LL MOD;
+const int dim = 3; // dimension;
+const mat Id = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}; 
+// modify it if diemnsion != 2
+LL MOD = 1e9 + 7;
 mat mul(mat A, mat B) {
-  mat ans(dim, vector<LL>(dim, 0));
-  for (int k = 0; k < dim; ++k)
-    for (int i = 0; i < dim; ++i)
-      for (int j = 0; j < dim; ++j)
+  assert(A[0].size() == B.size());
+  mat ans(A.size(), vector<LL>(B[0].size(), 0));
+  for (int k = 0; k < B.size(); ++k)
+    for (int i = 0; i < A.size(); ++i)
+      for (int j = 0; j < B[0].size(); ++j)
         (ans[i][j] += A[i][k] * B[k][j]) %= MOD;
   return ans;
 }
-mat mat_pow(mat A, int k) {
+mat mat_pow(mat A, LL k) {
+  assert(A.size() == A[0].size());
   mat ans = Id;
   for (mat cur = A; k; k >>= 1, cur = mul(cur, cur))
     if (k&1) ans = mul(ans, cur);
   return ans;
 }
 void print_mat(mat A) {
-  for (int i = 0; i < dim; ++i) for (int j = 0; j < dim; ++j) 
-    cout << A[i][j] << " \n"[j == dim - 1];
+  for (int i = 0; i < A.size(); ++i) 
+    for (int j = 0; j < A[i].size(); ++j)
+      cout << A[i][j] << " \n"[j == A[i].size() - 1];
 }
