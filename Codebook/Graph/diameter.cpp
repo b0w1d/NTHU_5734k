1c9e8ae04e025aa0b19df4831d58b8b5abda87b9
rareone <rareone0602@me.com>
2018-07-24 19:55:36 +0800

    The diameter can compute ecc

--- Codebook/Graph/diameter.cpp
+++ Codebook/Graph/diameter.cpp
@@ -1,21 +1,45 @@
-struct diameter_solver{
-  vector<int> g[200000], d;
-  int n;
+const int SIZE = 1e6 + 10;
+struct Tree_ecc{
+  vector<pair<int, LL>> g[SIZE];
+  LL dp[SIZE][2] = {0}, ecc[SIZE];
+  int n = -1;
   void init(int _n) {
     n = _n;
-    d.resize(n);
+    for (int i = 0; i < n; ++i)
+      g[i].clear(), ecc[i] = dp[i][0] = dp[i][1] = 0;
   }
-  void add_edge(int v, int u) { // 0-index
-    g[v].push_back(u), g[u].push_back(v);
+  void add_edge(int v, int u, LL w) { // 0-index
+    g[u].emplace_back(v, w);
+    g[v].emplace_back(u, w);
   }
-  void dfs(int v = 0, int p = -1, int h = 0) {
-    d[v] = h;
-    for (int u: g[v]) if (p != u) dfs(u, v, h + 1);
+  void dfs_length(int v, int p) {
+    for (auto T: g[v]) {
+      int u; LL w;
+      tie(u, w) = T;
+      if (u == p) continue;
+      dfs_length(u, v);
+      LL length_from_u = dp[u][0] + w;
+      if (dp[v][0] < length_from_u)
+        dp[v][1] = dp[v][0], dp[v][0] = length_from_u;
+      else if (dp[v][1] < length_from_u)
+        dp[v][1] = length_from_u;
+    }
   }
-  int diameter() {
-    dfs();
-    int v = max_element(d.begin(), d.end()) - d.begin();
-    dfs(v);
-    return *max_element(d.begin(), d.end());
+  void dfs_ecc(int v, int p, LL pass_p) {
+    ecc[v] = max(dp[v][0], pass_p);
+    for (auto T: g[v]) {
+      int u; LL w;
+      tie(u, w) = T;
+      if (u == p) continue;
+      if (dp[u][0] + w == dp[v][0])
+        dfs_ecc(u, v, max(pass_p, dp[v][1]) + w);
+      else dfs_ecc(u, v, max(pass_p, dp[v][0]) + w);
+    }
   }
-}solver;
+  LL diameter() {
+    assert(~n);
+    dfs_length(0, 0);
+    dfs_ecc(0, 0, 0);
+    return *max_element(ecc, ecc + n);
+  }
+} solver;
