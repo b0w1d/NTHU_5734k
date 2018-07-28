1aef17b5bae9776f179999fc159c806c198b2a9b
rareone <rareone0602@me.com>
2018-07-28 14:14:16 +0800

    add number_theory functions, Ero's sieve CURRENTLY performance better than Aktin's... I'll try to optimize it.

--- Codebook/Math/basic.cpp
+++ Codebook/Math/basic.cpp
@@ -9,6 +9,12 @@ LL powmod(LL x, LL p, LL mod) {
     if (p&1) s = s * m % mod; // or consider int128
   return s;
 }
+LL LLmul(LL x, LL y, LL mod) {
+  LL ans = 0, m = abs(x), s = 0, sgn = (x > 0) xor (y > 0)? -1: 1;
+  for (x = abs(x), y = abs(y); y; y >>= 1, m <<= 1, m = m >= mod? m - mod: m)
+    if (y&1) s += m, s = s >= mod? s - mod: s;
+  return (s * sgn % mod + mod) % mod;
+}
 LL dangerous_mul(LL a, LL b, LL mod){ // 10 times faster than the above in average, but could be prone to wrong answer (extreme low prob?)
   return (a * b - (LL)((long double)a * b / mod) * mod) % mod;
 }
@@ -18,4 +24,15 @@ vector<LL> linear_inv(LL p, int k) { // take k
   for (int i = 2; i < inv.size(); ++i)
     inv[i] = (p - p / i) * inv[p % i] % p;
   return inv;
+}
+inline LL add(LL _x, LL _y, LL _mod = 1e9 + 7) {
+  ll _ = _x + _y;
+  if (_ >= _mod) _ -= _mod;
+  if (_ < 0) _ += mod;
+  return _;
+}
+inline LL mul(LL _x, LL _y, LL _mod = 1e9 + 7) {
+  ll _ = _x * _y;
+  if (_ >= _mod) _ %= _mod;
+  return _;
 }
