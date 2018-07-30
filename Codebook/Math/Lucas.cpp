03a124815b2ec7a5387984c12ebe0a9ce385f8d6
rareone <rareone0602@me.com>
2018-07-31 03:33:43 +0800

    Holy shoot! A critically fatal bug detected again in Lucas.cpp

--- Codebook/Math/Lucas.cpp
+++ Codebook/Math/Lucas.cpp
@@ -1,11 +1,11 @@
 LL fac[100000] = {1};
 LL C(LL a, LL b, LL p) {
   for (int i = 1; i <= p; ++i) fac[i] = fac[i - 1] * i % p;
-  LL ans = 0;
+  LL ans = 1;
   for (;a; a /= p, b /= p) {
     LL A = a % p, B = b % p;
     if (A < B) return 0;
-    (ans += fac[A] * powmod(fac[B] * fac[A - B] % p, p - 2, p) % p) %= p;
+    (ans *= fac[A] * powmod(fac[B] * fac[A - B] % p, p - 2, p) % p) %= p;
   }
   return ans;
 }
