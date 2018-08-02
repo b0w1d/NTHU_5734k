59a451ddfe5c00841a7737698feb52dd8f72bd14
rareone <rareone0602@me.com>
2018-08-02 22:09:59 +0800

    我在寫差分約束系統時更新的

--- Codebook/Graph/SPFA.cpp
+++ Codebook/Graph/SPFA.cpp
@@ -1,4 +1,5 @@
 struct SPFA {
+  const LL INF = 1ll<<62;
   vector<vector<pair<int, LL>>> g;
   vector<int> p;
   vector<LL> d;
@@ -6,22 +7,22 @@ struct SPFA {
   void init(int _n) {
     n = _n;
     g.assign(n, vector<pair<int, LL>>(0));
-    d.assign(n, 1ll << 62);
+    d.assign(n, INF);
     p.assign(n, -1);
   }
   void add_edge(int u, int v, LL w) {
-    g[v].push_back({u, w});
     g[u].push_back({v, w});
   }
   LL shortest_path(int s, int t) {
     for (int i = 0; i < n; ++i)
-      sort(g[i].begin(), g[i].end(), [](auto A, auto B) {
+      sort(g[i].begin(), g[i].end(), [](pair<int, LL> A, pair<int, LL> B) {
         return A.second < B.second;
       });
     vector<bool> inq(n, false);
+    vector<int> inq_t(n, 0);
     queue<int> q;
     q.push(s);
-    d[s] = 0;
+    d[s] = 0, inq_t[s] = 1;
     int u, v;
     LL w;
     while (q.size()) {
@@ -30,8 +31,10 @@ struct SPFA {
         tie(u, w) = P;
         if (d[u] > d[v] + w) {
           d[u] = d[v] + w, p[u] = v;
-          if (not inq[u])
-            q.push(u), inq[u] = true;
+          if (not inq[u]) {
+            q.push(u), inq[u] = true, ++inq_t[u];
+            if (inq_t[u] > n) return -INF;
+          }
         }
       }
     }
