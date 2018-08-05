82df48e7a196422112bf896207a39b8bf8cc0aab
rareone <rareone0602@me.com>
2018-08-05 23:37:08 +0800

    bug fixed in Prufer_code

--- Codebook/Graph/Prufer_code.cpp
+++ Codebook/Graph/Prufer_code.cpp
@@ -12,27 +12,30 @@ vector<int> Prufer_encode(vector<vector<int>> T) {
     --deg[v];
     for (int u: T[v]) {
       if (deg[u]) {
-        --deg[u], code.push_back(u); 
+        --deg[u];
+        code.push_back(u);
         if (deg[u] == 1) pq.push(u);
       }
     }
   }
   return code;
 }
-
 vector<vector<int>> Prufer_decode(vector<int> C) {
   int n = C.size() + 2;
   vector<vector<int>> T(n, vector<int>(0));
-  vector<int> deg(n - 1, 0); // outdeg
+  vector<int> deg(n, 1); // outdeg
   for (int c: C) ++deg[c];
-  set<int> S; 
-  for (int i = 0; i < n - 1; ++i) 
-    if (not deg[i]) S.insert(i);
+  priority_queue<int, vector<int>, greater<int>> q;
+  for (int i = 0; i < n; ++i) if (deg[i] == 1) q.push(i);
   for (int c: C) {
-    int v = *S.begin(); S.erase(S.begin());
+    int v = q.top(); q.pop();
     T[v].push_back(c), T[c].push_back(v);
     --deg[c];
-    if (not deg[c]) S.insert(c);
+    --deg[v];
+    if (deg[c] == 1) q.push(c);
   }
+  int u = find(deg.begin(), deg.end(), 1) - deg.begin();
+  int v = find(deg.begin() + u + 1, deg.end(), 1) - deg.begin();
+  T[u].push_back(v), T[v].push_back(u);
   return T;
 }
