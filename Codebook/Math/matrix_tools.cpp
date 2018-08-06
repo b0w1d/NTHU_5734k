37007793e9206ae5143fe1c7fe05778f9d9d00a5
rareone <rareone0602@me.com>
2018-08-06 23:39:24 +0800

    update

--- Codebook/Math/matrix_tools.cpp
+++ Codebook/Math/matrix_tools.cpp
@@ -24,3 +24,37 @@ void print_mat(mat A) {
     for (int j = 0; j < A[i].size(); ++j)
       cout << A[i][j] << " \n"[j == A[i].size() - 1];
 }
+pair<LL, LL> Gaussian_elimination(mat &A) {
+  // upper triangular decomposition
+  assert(A[0].size() == A.size());
+  LL det = 1, rank = 0, n = A.size();
+  for (int i = 0; i < n; ++i) {
+    int sw = find_if(A.begin() + i, A.end(),
+                     [=](vector<LL> col) { return col[i];}) - A.begin();
+    if (sw == n) { det = 0; continue; }
+    if (sw != i) (det *= MOD - 1) %= MOD, swap(A[sw], A[i]);
+    for (int j = i + 1; j < n; ++j) {
+      if (A[j][i]) for (int k = n - 1; k >= i; --k) {
+        A[j][k] -= A[i][k] * powmod(A[j][i], MOD - 2, MOD) % MOD;
+        A[j][k] = (A[j][k] % MOD + MOD) % MOD;
+      }
+    }
+    det *= A[i][i], ++rank;
+  }
+  return {det, rank};
+}
+typedef vector<vector<LL> > mat;
+LL permanent(mat A) {
+  LL n = A.size(), ans = 0, *tmp = new LL[n], add;
+  for (int pgray = 0, s = 1, gray, i; s < 1 << n; ++s) {
+    gray = s ^ s >> 1, add = 1;
+    i = __builtin_ctz(pgray ^ gray);
+    for (int j = 0; j < n; ++j) 
+      add *= tmp[j] += A[i][j] * (gray>>i&1 ? 1 : -1);
+    ans += add * (s&1^n&1? -1 : 1), pgray = gray;
+  }
+  return ans;
+}
+// how many ways to put rooks on a matrix with 0,1 as constrain
+// 1 - ok to put
+// 0 - not ok to put
