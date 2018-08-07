42c2ef807c9ac212830ddaeb76b5a2d35c97cada
jjjjj19980806 <jjjjj19980806@gmail.com>
2018-08-07 18:44:33 +0800

    Update Biconnected component.cpp

--- Codebook/Graph/Biconnected component.cpp
+++ Codebook/Graph/Biconnected component.cpp
@@ -8,13 +8,11 @@ class biconnected_component {
   vector< vector<int> > comp;
   vector<int> pre, depth;
   int n;
-  int iter;
 
   biconnected_component(int _n) : n(_n) {
     depth = vector<int>(n, -1);
     pre.resize(n);
     g.resize(n);
-    iter = 0;
   }
 
   void add(int u, int v) {
@@ -24,16 +22,17 @@ class biconnected_component {
   }
 
   int dfs(int v, int pa, int d) {
-    pre[iter++] = v;
     depth[v] = d;
+    pre.push_back(v);
     for (int u : g[v]) {
       if (u == pa) continue;
       if (depth[u] == -1) {
         int child = dfs(u, v, depth[v] + 1);
         if (child >= depth[v]) {
           comp.push_back(vector<int>(1, v));
-          while (v != pre[iter - 1]) {
-            comp.back().push_back(pre[--iter]);
+          while (pre.back() != v) {
+            comp.back().push_back(pre.back());
+            pre.pop_back();
           }
         }
         d = min(d, child);  
@@ -69,31 +68,3 @@ class biconnected_component {
     return res;
   }
 };
-
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  int n, m, u, v;
-  bool newline = false;
-  while (cin >> n >> m) {
-    if (n == 0 && m == 0) break;
-    if (newline) cout << "\n";
-    newline = true;
-    biconnected_component bcc(n);
-    for (int i = 0; i < m; i++) {
-      cin >> u >> v;
-      bcc.add(u, v);
-    }
-    bcc.solve();
-    vector<int> ans = bcc.get_ap();
-    if (ans.size() == 0) {
-      cout << "None.\n";
-    }
-    else {
-      for (int i : ans) {
-        cout << i << "\n";
-      }
-    }
-  }
-  return 0;
-}
