23ae09c279bd21979f8b8cd7f69db4f6318b5811
rareone <rareone0602@me.com>
2018-08-18 03:38:03 +0800

    add pull

--- Codebook/DS/RMQs_and_Segs/Sgt_sum.cpp
+++ Codebook/DS/RMQs_and_Segs/Sgt_sum.cpp
@@ -2,11 +2,15 @@ struct seg{
   LL sum = 0, lazy = 0;
   seg* ch[2] = {nullptr, nullptr};
   int l, r, m;
+  void pull() {
+    sum = ch[0] -> sum + ch[1] -> sum;
+  }
   seg(int l, int r) : l(l), r(r) {
     if (r - l > 1) {
       m = l + r >> 1;
       ch[0] = new seg(l, m);
       ch[1] = new seg(m, r);
+      pull();
     }
   }
   void reset() {
@@ -18,9 +22,6 @@ struct seg{
       ch[i] -> sum += lazy * (ch[i] -> r - ch[i] -> l),
       ch[i] -> lazy += lazy;
     lazy = 0;
-  }
-  void pull() {
-    sum = ch[0] -> sum + ch[1] -> sum;
   }
   void add(int L, int R, LL val) {
     if (L >= R) return;
