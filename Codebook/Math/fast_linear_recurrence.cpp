27a5066b22ae3b9bfeba00711d65452d280d522e
b0w1d <61a5t0@gmail.com>
2018-08-26 07:09:26 +0800

    Update fast_linear_recurrence.cpp

--- Codebook/Math/fast_linear_recurrence.cpp
+++ Codebook/Math/fast_linear_recurrence.cpp
@@ -1,31 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> fast_linear_recurrence(const vector<int> &t, long long p, int mod) { // O(lg(p) * t.size()**2)
-  auto advance = [&](const vector<int> &u) {
-    vector<int> v(t.size());
-    v[0] = 1LL * u.back() * t[0] % mod;
-    for (int i = 1; i < t.size(); ++i) v[i] = (u[i - 1] + 1LL * u.back() * t[i]) % mod;
+template<typename T>
+vector<T> fast_linear_recurrence(const vector<T> &t, long long p) { // O(lg(p) * t.size()**2)
+  auto advance = [&](const vector<T> &u) {
+    vector<T> v(t.size());
+    v[0] = u.back() * t[0];
+    for (int i = 1; i < t.size(); ++i) v[i] = u[i - 1] + u.back() * t[i];
     return v;
   };
 
-  vector<vector<int>> kk(t.size(), vector<int>(t.size())); // kk[i] = lambda(t ** (t.size + i))
-  for (int i = 0; i < t.size(); ++i) kk[i] = i ? advance(kk[i - 1]) : t;
+  vector<vector<T>> kk(2 * t.size(), vector<T>(t.size())); // kk[i] = lambda(t ** i)
+  kk[0][0] = 1;
+  for (int i = 1; i < 2 * t.size(); ++i) kk[i] = advance(kk[i - 1]);
+  if (p < kk.size()) return kk[p];
 
-  auto square = [&](const vector<int> &u) {
-    vector<int> v(2 * t.size());
+  auto square = [&](const vector<T> &u) {
+    vector<T> v(2 * t.size());
     for (int j = 0; j < u.size(); ++j)
       for (int k = 0; k < u.size(); ++k)
-        v[j + k] = (v[j + k] + 1LL * u[j] * u[k]) % mod;
+        v[j + k] = v[j + k] + u[j] * u[k];
     for (int j = u.size(); j < v.size(); ++j)
       for (int k = 0; k < u.size(); ++k)
-        v[k] = (v[k] + 1LL * v[j] * kk[j - u.size()][k]) % mod;
+        v[k] = v[k] + v[j] * kk[j][k];
     v.resize(u.size());
     return v;
   };
 
-  vector<int> m(t.size());
-  m[1] = 1; // T[0] = {0, 1, 0, 0, ...}
+  vector<T> m(kk[1]);
   for (int i = 62 - __builtin_clzll(p); ~i; --i) {
     m = square(m);
     if (p >> i & 1) m = advance(m);
@@ -34,17 +36,14 @@ vector<int> fast_linear_recurrence(const vector<int> &t, long long p, int mod) {
   return m;
 }
 
-signed main() { // Project Euler 258, 3833 ms on CF
-  int mod = 20092010;
+signed main() { // 405 ms on CF
   vector<int> t(2000);
   t[0] = t[1] = 1; // f[i] = f[i - 2000] + f[i - 1999]
-  auto m = fast_linear_recurrence(t, (long long) 1e18, mod);
+  auto m = fast_linear_recurrence<int>(t, (long long) 1e18);
 
   vector<int> v(2000, 1); // f[i] = 1 for i < 2000
   int res = 0;
-  for (int i = 0; i < m.size(); ++i)
-    res = (res + 1LL * v[i] * m[i]) % mod;
-  if (res < 0) res += mod;
+  for (int i = 0; i < m.size(); ++i) res += v[i] * m[i];
   cout << res << endl;
 
   return 0;
