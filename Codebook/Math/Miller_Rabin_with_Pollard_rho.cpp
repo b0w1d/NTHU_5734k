a3e7e540dc56827e77328d8bef5848fbfcbd19d9
rareone <rareone0602@me.com>
2018-08-29 19:14:34 +0800

    c++11-ized miller_rabin with pollard rho

--- Codebook/Math/Miller_Rabin_with_Pollard_rho.cpp
+++ Codebook/Math/Miller_Rabin_with_Pollard_rho.cpp
@@ -1,34 +1,41 @@
-// Miller_Rabin
-LL abs(LL a) {return a > 0? a: -a;}
-bool witness(LL a, LL n, LL u, int t) {
-  LL x = modpow(a, u, n), nx;
-  for (int i = 0; i < t; ++i, x = nx){
-    nx = mul(x, x, n);
-    if (nx == 1 and x != 1 and x != n - 1) return 1;
-  }
-  return x != 1;
-}
-const LL wits[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 bool miller_rabin(LL n, int s = 7) {
+  const LL wits[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  auto witness = [=](LL a, LL n, LL u, int t) {
+    LL x = powmod(a, u, n), nx; // use LLmul, remember
+    for (int i = 0; i < t; ++i, x = nx){
+      nx = LLmul(x, x, n);
+      if (nx == 1 and x != 1 and x != n - 1) return true;
+    }
+    return x != 1;
+  };
   if (n < 2) return 0;
   if (n&1^1) return n == 2;
   LL u = n - 1, t = 0, a; // n == (u << t) + 1
   while (u&1^1) u >>= 1, ++t;
-  while (s--) 
-    if (a = wits[s] % n and witness(a, n, u, t)) return 0;
+  while (s--)
+    if ((a = wits[s] % n) and witness(a, n, u, t)) return 0;
   return 1;
 }
 // Pollard_rho
-LL f(LL x, LL n) {
-  return mul(x, x, n) + 1;
-}
 LL pollard_rho(LL n) {
+  auto f = [=](LL x, LL n) { return LLmul(x, x, n) + 1; };
   if (n&1^1) return 2;
   while (true) {
     LL x = rand() % (n - 1) + 1, y = 2, d = 1;
-    for (int sz = 2; d == 1; y = x, sz <<= 1) 
-      for (int i = 0; i < sz and d <= 1; ++i) 
+    for (int sz = 2; d == 1; y = x, sz <<= 1)
+      for (int i = 0; i < sz and d <= 1; ++i)
         x = f(x, n), d = __gcd(abs(x - y), n);
     if (d and n - d) return d;
   }
+}
+vector<pair<LL, int>> factor(LL m) {
+  vector<pair<LL, int>> ans;
+  while (m != 1) {
+    LL cur = m;
+    while (not miller_rabin(cur)) cur = pollard_rho(cur);
+    ans.emplace_back(cur, 0);
+    while (m % cur == 0) ++ans.back().second, m /= cur;
+  }
+  sort(ans.begin(), ans.end());
+  return ans;
 }
--- Codebook/Math/basic.cpp
+++ Codebook/Math/basic.cpp
@@ -10,10 +10,10 @@ LL powmod(LL x, LL p, LL mod) {
   return s;
 }
 LL LLmul(LL x, LL y, LL mod) {
-  LL ans = 0, m = abs(x), s = 0, sgn = (x > 0) xor (y > 0)? -1: 1;
-  for (x = abs(x), y = abs(y); y; y >>= 1, m <<= 1, m = m >= mod? m - mod: m)
+  LL m = x, s = 0;
+  for (; y; y >>= 1, m <<= 1, m = m >= mod? m - mod: m)
     if (y&1) s += m, s = s >= mod? s - mod: s;
-  return (s * sgn % mod + mod) % mod;
+  return s;
 }
 LL dangerous_mul(LL a, LL b, LL mod){ // 10 times faster than the above in average, but could be prone to wrong answer (extreme low prob?)
   return (a * b - (LL)((long double)a * b / mod) * mod) % mod;
@@ -24,15 +24,4 @@ vector<LL> linear_inv(LL p, int k) { // take k
   for (int i = 2; i < inv.size(); ++i)
     inv[i] = (p - p / i) * inv[p % i] % p;
   return inv;
-}
-inline LL add(LL _x, LL _y, LL _mod = 1e9 + 7) {
-  ll _ = _x + _y;
-  if (_ >= _mod) _ -= _mod;
-  if (_ < 0) _ += mod;
-  return _;
-}
-inline LL mul(LL _x, LL _y, LL _mod = 1e9 + 7) {
-  ll _ = _x * _y;
-  if (_ >= _mod) _ %= _mod;
-  return _;
 }
