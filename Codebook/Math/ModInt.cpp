6359ef5f09297cfd7a5b8e866dc4eeab0e64703a
b0w1d <61a5t0@gmail.com>
2018-09-03 03:03:39 +0800

    Update ModInt.cpp

--- Codebook/Math/ModInt.cpp
+++ Codebook/Math/ModInt.cpp
@@ -6,11 +6,22 @@ struct ModInt {
   ModInt &operator=(int v) { return val = (v % mod + mod) % mod, *this; }
   ModInt &operator=(const ModInt &oth) { return val = oth.val, *this; }
   ModInt operator+(const ModInt &oth) const {
-    int u = val + oth.val;
+    int u = (val + oth.val) % mod;
+    return u < 0 ? u + mod : u;
+  }
+  ModInt operator-(const ModInt &oth) const {
+    int u = (val - oth.val) % mod;
     return u < 0 ? u + mod : u;
   }
   ModInt operator*(const ModInt &oth) const {
     return 1LL * val * oth.val % mod;
   }
-  ModInt operator-() const { return val > 0 ? mod - val : 0; }
+  ModInt operator/(const ModInt &oth) const {
+    function<int(int, int, int, int)> modinv = [&](int a, int b, int x, int y) {
+      if (b == 0) return x < 0 ? x + mod : x;
+      return modinv(b, a - a / b * b, y, x - a / b * y);
+    };
+    return *this * modinv(oth.val, mod, 1, 0);
+  }
+  ModInt operator-() const { return mod - val; }
 };
