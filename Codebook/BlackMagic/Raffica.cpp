374dcff2ea9712af458f3db0b2e488adad0c9ccc
b0w1d <61a5t0@gmail.com>
2018-09-15 20:11:42 +0800

    Update Raffica.cpp

--- Codebook/BlackMagic/Raffica.cpp
+++ Codebook/BlackMagic/Raffica.cpp
@@ -1,7 +1,7 @@
-struct LCT { /* expected O(m + n); CF: N=6e6, M=6e6 random graph < 700ms */
+struct LCT {
   struct Node {
     int fa, val, dpt;
-    vector<int> ch;
+    set<int> ch;
     Node() : fa(-1), val(0), dpt(0) {}
   };
   vector<Node> nodes;
@@ -10,15 +10,15 @@ struct LCT { /* expected O(m + n); CF: N=6e6, M=6e6 random graph < 700ms */
 
   void modify(int x, int v) {
     nodes[x].val = v;
-    for (int i = 0; i < nodes[x].ch.size(); ++i) {
-      nodes[nodes[x].ch[i]].val = v;
-      modify(nodes[x].ch[i], v);
+    for (int y : nodes[x].ch) {
+      nodes[y].val = v;
+      modify(y, v);
     }
   }
   void maintain(int x) {
-    for (int i = 0; i < nodes[x].ch.size(); ++i) {
-      nodes[nodes[x].ch[i]].dpt = nodes[x].dpt + 1;
-      maintain(nodes[x].ch[i]);
+    for (int y : nodes[x].ch) {
+      nodes[y].dpt = nodes[x].dpt + 1;
+      maintain(y);
     }
   }
   int query(int x) { return nodes[x].val; }
@@ -26,32 +26,26 @@ struct LCT { /* expected O(m + n); CF: N=6e6, M=6e6 random graph < 700ms */
     nodes[x].fa = y;
     nodes[x].dpt = nodes[y].dpt + 1;
     maintain(x);
-    nodes[y].ch.emplace_back(x);
+    nodes[y].ch.emplace(x);
   }
   void cut(int x) {
     if (nodes[x].fa == -1) return;
     Node &s = nodes[nodes[x].fa];
     nodes[x].dpt = 0;
     maintain(x);
-    for (int i = 0; i < s.ch.size(); ++i) {
-      if (s.ch[i] == x) {
-        for (int j = i; j < s.ch.size() - 1; ++j) s.ch[j] = s.ch[j + 1];
-        s.ch.pop_back();
-        break;
-      }
-    }
+    s.ch.erase(x);
     nodes[x].fa = -1;
   }
   bool lca(int x, int y) {
     if (x == y) return true;
-    for (int i = 0; i < nodes[x].ch.size(); ++i)
-      if (lca(nodes[x].ch[i], y)) return true;
+    for (int z : nodes[x].ch)
+      if (lca(z, y)) return true;
     return false;
   }
 };
 
 template<typename max_type = int, typename weight_type = int, max_type inf = 0x3f3f3f3f>
-vector<max_type> raffica(const vector<vector<pair<weight_type, int>>> &g, int src) {
+vector<max_type> SPFA(const vector<vector<pair<weight_type, int>>> &g, int src) {
   LCT tree(g.size());
   queue<int> que;
   vector<max_type> d(g.size(), inf);
