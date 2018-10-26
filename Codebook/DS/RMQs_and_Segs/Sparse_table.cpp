d85b88f4078a67735b87af5d3464642442d075a6
rareone <rareone0602@me.com>
2018-10-26 16:47:32 +0800

    template sparse table

--- Codebook/DS/RMQs_and_Segs/Sparse_table.cpp
+++ Codebook/DS/RMQs_and_Segs/Sparse_table.cpp
@@ -1,15 +1,17 @@
-const int INF = 1e9 + 10;
-vector<vector<int>> build_sparse_table(const vector<int> &A) {
+template <class T>
+vector<vector<T>> build_sparse_table(const vector<T> &A) {
   int n = A.size(), lg = 31 - __builtin_clz(n);
-  vector<vector<int>> st(lg + 1, vector<int>(n));
+  vector<vector<T>> st(lg + 1, vector<T>(n));
   for (int i = 0; i < n; ++i) st[0][i] = A[i];
   for (int j = 1; (1<<j) <= n; ++j)
     for (int i = 0; i + (1<<j) <= n; ++i)
-      st[j][i] = min(st[j - 1][i], st[j - 1][i + (1<<(j - 1))]);
+      st[j][i] = max(st[j - 1][i], st[j - 1][i + (1<<(j - 1))]);
   return st;
 }
-int sparse_rmq(int i, int j, vector<vector<int>> &__ST;) {
-  if (i >= j) return INF;
+
+template <class T>
+T sparse_rmq(int i, int j, vector<vector<T>> &__ST) {
+  assert(i < j);
   int n = __ST[0].size(), k = 31 - __builtin_clz(j - i);
-  return min(__ST[k][i], __ST[k][j - (1<<k)]);
+  return max(__ST[k][i], __ST[k][j - (1<<k)]);
 }
