4e0c092ef8a4f7a683fdb8f3ad0a6c1ab4356b9b
rareone <rareone0602@me.com>
2019-01-07 23:41:16 +0800

    準到很噁心的積分

--- Codebook/Math/integration.cc
+++ Codebook/Math/integration.cc
@@ -2,23 +2,24 @@
 using namespace std;
 class Integration {
   typedef long double Double;
-  Double ALPHA = sqrt((5 + sqrt(11)) / 12);
-  Double BETA  = sqrt((5 - sqrt(11)) / 12);
+  Double ALPHA = sqrt((5 - sqrt(40. / 7))) / 3, WA = (322 + sqrt(11830)) / 900;
+  Double W0 = 128. / 225.;
+  Double BETA  = sqrt((5 + sqrt(40. / 7))) / 3, WB = (322 - sqrt(11830)) / 900;
   function<Double(Double)> f;
   Double quadrature(Double l, Double r) {
     auto m = (l + r) / 2, len = r - m;
-    return (f(m - ALPHA * len) + f(m - BETA * len) + f(m) +
-            f(m + ALPHA * len) + f(m + BETA * len)) * (r - l) / 5;
+    return (f(m - ALPHA * len) * WA + f(m - BETA * len) * WB + f(m) * W0 +
+            f(m + ALPHA * len) * WA + f(m + BETA * len) * WB) * len;
   }
   Double askArea(Double l, Double r, Double exceptArea) {
     Double m = (l + r) / 2, L = quadrature(l, m), R = quadrature(m, r);
-    if (exceptArea < 1e-20 or abs((L + R - exceptArea) / exceptArea) < 1e-6) 
+    if (exceptArea < 1e-15 or abs((L + R - exceptArea) / exceptArea) < 1e-6)
       return L + R;
     else return askArea(l, m, L) + askArea(m, r, R);
   }
 public:
   Integration(function<Double(Double)> func) : f(func) {}
-  Double intergal(Double l, Double r, int piece = 1e6) {
+  Double intergal(Double l, Double r, int piece = 1e5) {
     Double ans = 0;
     for (Double dx = (r - l) / piece, i = 0; i < piece; ++i) {
       auto cur = l + dx * i;
