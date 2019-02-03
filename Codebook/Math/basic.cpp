c97c04dc53137a2dee79c0714c6e653e00999d94
b0w1d <61a5t0@gmail.com>
2019-02-04 00:32:53 +0800

    Update basic.cpp

--- Codebook/Math/basic.cpp
+++ Codebook/Math/basic.cpp
@@ -25,3 +25,10 @@ vector<LL> linear_inv(LL p, int k) { // take k
     inv[i] = (p - p / i) * inv[p % i] % p;
   return inv;
 }
+
+tuple<int, int, int> ext_gcd(int a, int b) {
+  if (!b) return {1, 0, a};
+  int x, y, g;
+  tie(x, y, g) = ext_gcd(b, a % b);
+  return {y, x - a / b * y, g};
+}
