88c00e47dc96fb48b57eefaf4855a84f0c349615
rareone <rareone0602@me.com>
2019-02-07 04:42:39 +0800

    int implemented hashing

--- Codebook/String/hashing.cpp
+++ Codebook/String/hashing.cpp
@@ -1,20 +1,22 @@
-template<class C>
+const int primes[10] = {1000010483, 1000010503, 1000010513, 1000010549, 1000010593, 1000010597, 1000010611, 1000010633, 1000010659, 1000010699};
+template<class F>
 struct Hashing { // As default, SIGMA = [a-z]
-  vector<LL> H, q_inv_pow;
-  const LL primes[10] = {1730472629, 1730472637, 1730472673, 1730472677, 1730472697,
-  1730472707, 1730472719, 1730472761, 1730472769, 1730472787};
-  LL P, Q;
+  vector<int> H, q_inv;
+  int MOD, Q;
   Hashing() {}
-  Hashing(C s, int ch, LL SIGMA_SIZE = 1e8) {
-    int n = s.size();
-    H.resize(n + 1), q_inv_pow.resize(n + 1);
-    P = primes[ch%10], Q = SIGMA_SIZE + 1;
-    q_inv_pow[0] = 1, q_inv_pow[1] = powmod(Q, P - 2, P);
-    for (int i = 2; i <= n; ++i) q_inv_pow[i] = q_inv_pow[i - 1] * q_inv_pow[1] % P;
-    for (LL i = 0, col = 1; i < n; ++i, col = col * Q % P)
-      (H[i + 1] += (H[i] + col * s[i])) %= P;
+  Hashing(F s, int P, int Q = 256) : MOD(P), Q(Q) {
+    function<int(int)> inv = [&](int a) {
+      return a == 1 ? 1 : 1ll * (MOD - MOD / a) * inv(MOD % a) % MOD;
+    };
+    const int n = s.size(), invQ = inv(Q);
+    H.assign(n + 1, 0), q_inv.resize(n + 1), q_inv[0] = 1;
+    partial_sum(q_inv.begin(), q_inv.end(), q_inv.begin(), [&](int prev, int _) {
+      return 1ll * prev * invQ % MOD;
+    });
+    for (int i = 0, col = 1; i < n; ++i, col = 1ll * col * Q % P)
+      H[i + 1] = (H[i] + 1ll * col * s[i]) %= P;
   }
-  LL get_hash(int l, int r) { // [l, r)
-    return (P + H[r] - H[l]) * q_inv_pow[l] % P;
+  int get_hash(int l, int r) { // [l, r)
+    return 1ll * (MOD + H[r] - H[l]) * q_inv[l] % MOD;
   }
 };
