88f753cd9f2bb29ce3546b586a2e375fada7c65a
rareone <rareone0602@me.com>
2019-02-09 23:28:27 +0800

    add scc tool

--- Codebook/Graph/scc.cpp
+++ Codebook/Graph/scc.cpp
@@ -1,73 +1,65 @@
-#include <bits/stdc++.h>
+class Kosaraju {
 
-using namespace std;
-
-class SCC {
-  public:
-  vector< vector<int> > g, rg;
-  vector<int> visit, was;
-  vector<int> id;
+  vector<vector<int>> g, rg, compo;
+  vector<int> order, DAGID;
+  vector<bool> vis;
   int n, iter;
 
-  SCC(int _n) : n(_n) {
-    g.resize(n);
+  void make_rg() {
+    for (int u = 0; u < n; ++u) for (int v : g[u]) rg[v].push_back(u);
+  }
+
+  void dfs_all() {
+    function<void(int)> dfs = [&](int u) {
+      vis[u] = true;
+      for (int v : g[u]) if (not vis[v]) dfs(v);
+      order.emplace_back(u);
+    };
+    for (int i = 0; i < n; ++i) if (not vis[i]) dfs(i);
+  }
+
+  void rdfs_all() {
+    function<void(int)> rdfs = [&](int u) {
+      DAGID[u] = iter;
+      for (int v : rg[u]) if (DAGID[v] == -1) rdfs(v);
+      compo.back().push_back(u);
+    };
+    for (int u : order) if (DAGID[u] == -1) {
+      compo.push_back(vector<int>(0));
+      rdfs(u), ++iter;
+    }
+  }
+
+public:
+
+  // remember that the graph is directed
+  Kosaraju(vector<vector<int>> &_g) : n(_g.size()), g(_g) {
     rg.resize(n);
-    was = vector<int>(n, 0);
-    id = vector<int>(n, -1);
+    compo.clear();
+    make_rg();
+    vis.assign(n, false);
+    DAGID.assign(n, -1);
     iter = 0;
+
+    dfs_all();
+    reverse(order.begin(), order.end());
+    rdfs_all();
   }
 
-  void add(int from, int to) {
-    assert(from >= 0 && from < n && to >= 0 && to < n);
-    g[from].emplace_back(to);
-    rg[to].emplace_back(from);
-  }
+  const vector<vector<int>>& get_components() { return compo; }
 
-  void dfs(int v) {
-    was[v] = true;
-    for (int u : g[v]) {
-      if (!was[u]) dfs(u);
-    }
-    visit.emplace_back(v);
-  }
-
-  void rdfs(int v) {
-    id[v] = iter;
-    for (int u : rg[v]) {
-      if (id[u] == -1) rdfs(u);
-    }
-  }
-
-  int solve() {
-    for (int i = 0; i < n; i++) {
-      if (!was[i]) dfs(i);
-    }
-    for (int i = n - 1; i >= 0; i--) {
-      if (id[ visit[i] ] == -1) {
-        rdfs(visit[i]);
-        iter++;
+  const vector<vector<int>> get_condensed_DAG(bool simple = true) {
+    vector<vector<int>> ret(iter);
+    for (int i = 0; i < iter; ++i) {
+      for (int u : compo[i]) for (int v : g[u]) if (DAGID[v] != i) {
+        ret[i].push_back(DAGID[v]);
+      }
+      if (simple) {
+        sort(ret[i].begin(), ret[i].end());
+        ret[i].resize(unique(ret[i].begin(), ret[i].end()) - ret[i].begin());
       }
     }
-    return iter;
+    return ret;
   }
 };
 
-/*
-  Kosaraju's algorithm
-  solve() return the number of scc
-  node with same id[] belong the same scc
-*/
-
-int main() {
-  int n, m;
-  while (cin >> n >> m && n && m) {
-    SCC scc(n);
-    for (int i = 0; i < m; i++) {
-      int u, v;
-      cin >> u >> v;
-      scc.add(u, v);
-    }
-    cout << scc.solve() << "\n";
-  }
-  return 0;
-}
