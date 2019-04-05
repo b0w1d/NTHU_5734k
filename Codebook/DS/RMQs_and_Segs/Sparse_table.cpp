82a9f47db3da6d8d4c401b15c6bc12906e26af17
rareone0602 <rareone0602@rareone0602s-MacBook-Pro.local>
2019-04-05 23:43:01 +0800

    structify the sparse table

--- Codebook/DS/RMQs_and_Segs/Sparse_table.cpp
+++ Codebook/DS/RMQs_and_Segs/Sparse_table.cpp
@@ -1,17 +1,23 @@
 template <class T>
-vector<vector<T>> build_sparse_table(const vector<T> &A) {
-  int n = A.size(), lg = 31 - __builtin_clz(n);
-  vector<vector<T>> st(lg + 1, vector<T>(n));
-  for (int i = 0; i < n; ++i) st[0][i] = A[i];
-  for (int j = 1; (1<<j) <= n; ++j)
-    for (int i = 0; i + (1<<j) <= n; ++i)
-      st[j][i] = max(st[j - 1][i], st[j - 1][i + (1<<(j - 1))]);
-  return st;
-}
-
-template <class T>
-T sparse_rmq(int i, int j, vector<vector<T>> &__ST) {
-  assert(i < j);
-  int n = __ST[0].size(), k = 31 - __builtin_clz(j - i);
-  return max(__ST[k][i], __ST[k][j - (1<<k)]);
-}
+struct SparseTable {
+  vector<vector<T>> st;
+  function<T(T, T)> op;
+  int n;
+  SparseTable(const vector<T> &A, const function<T(T, T)> binaryOp) {
+    n = A.size(), op = binaryOp;
+    int lg = 31 - __builtin_clz(n);
+    st.assign(lg + 1, vector<T>(n));
+    for (int i = 0; i < n; ++i) st[0][i] = A[i];
+    for (int j = 1; (1<<j) <= n; ++j) for (int i = 0; i + (1<<j) <= n; ++i)
+      st[j][i] = binaryOp(st[j - 1][i], st[j - 1][i + (1<<(j - 1))]);
+  }
+  T rmq(int i, int j) {
+    assert(i < j);
+    int k = 31 - __builtin_clz(j - i);
+    return op(st[k][i], st[k][j - (1<<k)]);
+  }
+  /*
+  Exmaple for calling contructor
+  SparseTable ST(A, [](int a, int b) { return max(a, b); });
+  */
+};
