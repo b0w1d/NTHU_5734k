a651eb4f04a0a6d4c7d7ae4e1c45c4f47f7e3aa2
b0w1d <61a5t0@gmail.com>
2019-07-22 23:06:26 +0800

    Update gusfield.cpp

--- Codebook/String/gusfield.cpp
+++ Codebook/String/gusfield.cpp
@@ -2,20 +2,18 @@ template<typename T>
 vector<int> gusfield(const T &s) {
   vector<int> z(s.size(), s.size()); // z[i] := max k for z[0, k) = z[i, i + k)
   for (int i = 1, L = 0, R = 0; i < s.size(); ++i) {
-    if (R < i) {
+    if (R <= i) {
       L = R = i;
       while (R < s.size() && s[R] == s[R - L]) ++R;
       z[i] = R - L;
-      --R;
     } else {
       int k = i - L;
-      if (z[k] < R - i + 1) {
+      if (z[k] < R - i) {
         z[i] = z[k];
       } else {
         L = i;
         while (R < s.size() && s[R] == s[R - L]) ++R;
         z[i] = R - L;
-        --R;
       }
     }
   }
