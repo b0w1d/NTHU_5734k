707bed36b8606336f7f549775e1fc2a8809e5c92
rareone@users.noreply.github.com <rareone0602@rareone0602s-MacBook-Pro.local>
2019-08-10 12:35:53 +0800

    modify ModInt

--- Codebook/Math/ModInt.cpp
+++ Codebook/Math/ModInt.cpp
@@ -1,27 +1,30 @@
 template <int mod>
 struct ModInt {
   int val;
-  ModInt(int v = 0) : val((v % mod + mod) % mod) {}
-  ModInt(long long v) : val((v % mod + mod) % mod) {}
-  ModInt &operator=(int v) { return val = (v % mod + mod) % mod, *this; }
+  int trim(int x) const { return x >= mod ? x - mod : x < 0 ? x + mod : x; }
+  ModInt(int v = 0) : val(trim(v % mod)) {}
+  ModInt(long long v) : val(trim(v % mod)) {}
+  ModInt &operator=(int v) { return val = trim(v % mod), *this; }
   ModInt &operator=(const ModInt &oth) { return val = oth.val, *this; }
-  ModInt operator+(const ModInt &oth) const {
-    int u = (val + oth.val) % mod;
-    return u < 0 ? u + mod : u;
-  }
-  ModInt operator-(const ModInt &oth) const {
-    int u = (val - oth.val) % mod;
-    return u < 0 ? u + mod : u;
-  }
-  ModInt operator*(const ModInt &oth) const {
-    return 1LL * val * oth.val % mod;
-  }
+  ModInt operator+(const ModInt &oth) const { return trim(val + oth.val); }
+  ModInt operator-(const ModInt &oth) const { return trim(val - oth.val); }
+  ModInt operator*(const ModInt &oth) const { return 1LL * val * oth.val % mod; }
   ModInt operator/(const ModInt &oth) const {
     function<int(int, int, int, int)> modinv = [&](int a, int b, int x, int y) {
-      if (b == 0) return x < 0 ? x + mod : x;
+      if (b == 0) return trim(x);
       return modinv(b, a - a / b * b, y, x - a / b * y);
     };
     return *this * modinv(oth.val, mod, 1, 0);
   }
-  ModInt operator-() const { return mod - val; }
+  bool operator==(const ModInt &oth) const { return val == oth.val; }
+  ModInt operator-() const { return trim(mod - val); }
+  template<typename T> ModInt pow(T pw) {
+    bool sgn = false;
+    if (pw < 0) pw = -pw, sgn = true;
+    ModInt ans = 1;
+    for (ModInt cur = val; pw; pw >>= 1, cur = cur * cur) {
+      if (pw&1) ans = ans * cur;
+    }
+    return sgn ? ModInt{1} / ans : ans;
+  }
 };
