b899c14ca3741d63855dc6e3585018202b2c9099
rareone@users.noreply.github.com <rareone0602@rareone0602s-MacBook-Pro.local>
2019-08-13 16:11:07 +0800

    remove my implementation

--- Codebook/Math/discrete_log.cpp
+++ Codebook/Math/discrete_log.cpp
@@ -1,19 +1,3 @@
-LL discrete_log(LL b, LL p, LL n) {
-  map<LL, LL> att;
-  LL m = sqrt((double)p) + 1, M = powmod(b, m * (p - 2), p);
-  for (LL cur = 1, i = 0; i < m; ++i, cur = cur * b % p)
-    if (not att.count(cur)) att[cur] = i;
-  for (LL cur = 1, i = 0; i * m < p - 1; ++i, cur = cur * M % p) 
-    if (att.count(n * cur % p)) 
-      return (att[cur * n % p] + i * m) % (p - 1);
-  return -1;
-}
-// find x s.t. b**x % p == n with complexity O(sqrt(N))
-// return the smallest
-// return -1 if ans doesn't exist
-
-// ---------------------------------------------------------------
-
 int discrete_log(int a, int m, int p) { // a**x = m mod p
   int magic = sqrt(p) + 2;
   map<int, int> mp;
