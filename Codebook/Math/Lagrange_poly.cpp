18b8c401c4c40bdd5a3fe93eb90e9388c25e179d
rareone@users.noreply.github.com <rareone0602@rareone0602s-MacBook-Pro.local>
2019-08-13 15:29:55 +0800

    templatify Lagrange_poly

--- Codebook/Math/Lagrange_poly.cpp
+++ Codebook/Math/Lagrange_poly.cpp
@@ -1,20 +1,19 @@
+template<typename F>
 struct Lagrange_poly {
-  vector<LL> fac, p;
+  vector<F> fac, p;
   int n;
-  Lagrange_poly(vector<LL> p) : p(p) {
+  Lagrange_poly(vector<F> p) : p(p) { // f(i) = p[i]
     n = p.size();
     fac.resize(n), fac[0] = 1;
-    for (int i = 1; i < n; ++i) fac[i] = fac[i - 1] * i % MOD;
+    for (int i = 1; i < n; ++i) fac[i] = fac[i - 1] * F(i);
   }
-  LL solve(LL x) {
-    if (x < n) return p[x];
-    LL ans = 0, to_mul = 1;
-    for (int j = 0; j < n; ++j) (to_mul *= MOD - x + j) %= MOD;
+  F operator()(F x) const {
+    F ans(0), to_mul(1);
+    for (int j = 0; j < n; ++j) to_mul = to_mul * (F(j) - x);
+    assert(not(to_mul == F(0)));
     for (int j = 0; j < n; ++j) {
-      (ans += p[j] * to_mul % MOD *
-      powmod(MOD - x + j, MOD - 2, MOD) % MOD *
-      powmod(fac[n - 1 - j], MOD - 2, MOD) % MOD *
-      powmod(j&1? MOD - fac[j]: fac[j], MOD - 2, MOD)) %= MOD;
+      ans = ans + p[j] * to_mul / (F(j) - x) /
+            fac[n - 1 - j] / (j&1 ? -fac[j] : fac[j]);
     }
     return ans;
   }
