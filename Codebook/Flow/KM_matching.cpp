fd62712433103d838cb497822f82379048e1d92f
rareone@users.noreply.github.com <rareone0602@rareone0602s-MacBook-Pro.local>
2019-09-05 20:25:41 +0800

    tourist KM

--- Codebook/Flow/KM_matching.cpp
+++ Codebook/Flow/KM_matching.cpp
@@ -1,48 +1,68 @@
-const int MAXN = 1000;
-template <class TC>
-struct KM_matching { // if there's no edge, the weight is 0
-// complexity: O(n^3), support for negetive edge
-  int n, matchy[MAXN];
-  bool visx[MAXN], visy[MAXN];
-  TC adj[MAXN][MAXN], coverx[MAXN], covery[MAXN], slack[MAXN];
-  KM_matching(int _n) : n(_n) {
-    memset(matchy, -1, sizeof(matchy));
-    memset(covery, 0, sizeof(covery));
-    memset(adj, 0, sizeof(adj));
+template<typename T>
+struct Hungarian { // minimum weight matching
+  public:
+  int n, m;
+  vector< vector<T> > a;
+  vector<T> u, v;
+  vector<int> pa, pb, way;
+  vector<T> minv;
+  vector<bool> used;
+  T inf;
+ 
+  Hungarian(int _n, int _m) : n(_n), m(_m) {
+    assert(n <= m);
+    a = vector< vector<T> >(n, vector<T>(m));
+    v = u = vector<T>(n + 1);
+    pb = pa = vector<int>(n + 1, -1);
+    way = vector<int>(m, -1);
+    minv = vector<T>(m);
+    used = vector<bool>(m + 1);
+    inf = numeric_limits<T>::max();
   }
-  void add_edge(int x, int y, TC w) { adj[x][y] = w; }
-  bool aug(int u) {
-    visx[u] = true;
-    for (int v = 0; v < n; ++v)
-      if (not visy[v]) {
-        TC t = coverx[u] + covery[v] - adj[u][v];
-        if (t == 0) { // The edge is in Equality subgraph
-          visy[v] = true;
-          if (matchy[v] == -1 or aug(matchy[v])) 
-            return matchy[v] = u, true;
+ 
+  inline void add_row(int i) {
+    fill(minv.begin(), minv.end(), inf);
+    fill(used.begin(), used.end(), false);
+    pb[m] = i, pa[i] = m;
+    int j0 = m;
+    do {
+      used[j0] = true;
+      int i0 = pb[j0], j1 = -1;
+      T delta = inf;
+      for (int j = 0; j < m; j++) {
+        if (!used[j]) {
+          T cur = a[i0][j] - u[i0] - v[j];
+          if (cur < minv[j]) {
+            minv[j] = cur, way[j] = j0;
+          }
+          if (minv[j] < delta) {
+            delta = minv[j], j1 = j;
+          }
         }
-        else if (slack[v] > t) slack[v] = t;
       }
-    return false;
+      for (int j = 0; j <= m; j++) {
+        if (used[j]) {
+          u[pb[j]] += delta, v[j] -= delta;
+        } else {
+          minv[j] -= delta;
+        }
+      }
+      j0 = j1;
+    } while (pb[j0] != -1);
+    do {
+      int j1 = way[j0];
+      pb[j0] = pb[j1], pa[pb[j0]] = j0, j0 = j1;
+    } while (j0 != m);
   }
-  TC solve() {
-    for (int u = 0; u < n; ++u)
-      coverx[u] = *max_element(adj[u], adj[u] + n);
-    for (int u = 0; u < n; ++u) {
-      fill(slack, slack + n, INT_MAX);
-      while (memset(visx, 0, sizeof(visx)), 
-             memset(visy, 0, sizeof(visy)), 
-             not aug(u)) {
-        TC d = INT_MAX;
-        for (int v = 0; v < n; ++v) 
-          if (not visy[v]) d = min(d, slack[v]);
-        for (int v = 0; v < n; ++v) {
-          if (visx[v]) coverx[v] -= d;
-          if (visy[v]) covery[v] += d;
-        }
-      }
+ 
+  inline T current_score() {
+    return -v[m];
+  }
+ 
+  inline T solve() {
+    for (int i = 0; i < n; i++) {
+      add_row(i);
     }
-    return accumulate(coverx, coverx + n, (TC)0) + 
-           accumulate(covery, covery + n, (TC)0);
+    return current_score();
   }
-};
+}; 
