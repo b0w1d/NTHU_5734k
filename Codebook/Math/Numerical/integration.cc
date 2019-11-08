13b4d14f3dc5a7f2eca24ca17b5c8ae06f447ab1
rareone@users.noreply.github.com <rareone0602@rareone0602s-MacBook-Pro.local>
2019-11-08 12:21:43 +0800

    compiled

--- Codebook/Math/Numerical/integration.cc
+++ Codebook/Math/Numerical/integration.cc
@@ -1,7 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
+template<typename Double>
 class Integration {
-  typedef long double Double;
   Double ALPHA = sqrt((5 - sqrt(40. / 7))) / 3, WA = (322 + sqrt(11830)) / 900;
   Double W0 = 128. / 225.;
   Double BETA  = sqrt((5 + sqrt(40. / 7))) / 3, WB = (322 - sqrt(11830)) / 900;
@@ -13,13 +11,13 @@ class Integration {
   }
   Double askArea(Double l, Double r, Double exceptArea) {
     Double m = (l + r) / 2, L = quadrature(l, m), R = quadrature(m, r);
-    if (abs(L + R - exceptArea) < 1e-16)
+    if (abs(L + R - exceptArea) < 1e-10)
       return L + R;
     else return askArea(l, m, L) + askArea(m, r, R);
   }
 public:
   Integration(function<Double(Double)> func) : f(func) {}
-  Double intergal(Double l, Double r, int piece = 1e5) {
+  Double intergal(Double l, Double r, int piece = 10) {
     Double ans = 0;
     for (Double dx = (r - l) / piece, i = 0; i < piece; ++i) {
       auto cur = l + dx * i;
@@ -28,7 +26,3 @@ public:
     return ans;
   }
 };
-int main() {
-  Integration formula([](long double t) { return sqrt(1 - t * t); });
-  cout << fixed << setprecision(20) << 2 * formula.intergal(-1, 1) << '\n';
-}
